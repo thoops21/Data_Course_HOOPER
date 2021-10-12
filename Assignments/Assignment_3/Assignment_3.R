b5e47f87b763737c02b74ccb126b346d75e13494
thoops21 <89547520+thoops21@users.noreply.github.com>
2021-10-11 18:43:50 -0600

    Add files via upload

--- Assignments/Assignment_3/Assignment_3.R
+++ Assignments/Assignment_3/Assignment_3.R
@@ -16,10 +16,10 @@
 
 
 
-# Vector operations! ####
+# Vector operations! #### 
 
 # Vectors are 1-dimensional series of values in some order
-1:10 # ':' only works for integers
+1:10 # ':' only works for integers, this is a vector with 1 dimension
 letters # built-in pre-made vector of a - z
 
 
@@ -33,6 +33,16 @@ vector2 / 2
 vector1*vector2
 
 vector3 + 1 # can't add 1 to "a"
+# cant do this because we havent assigned a number value to a
+
+TF <- c(TRUE, FALSE, FALSE,TRUE)
+TF + 1
+mix <- c(TF,letters)
+
+chars <- c(letters, vector1)[30:33]
+
+as.numeric(chars)
+as.character(1:10)
 
 
 
@@ -42,6 +52,8 @@ data("iris") # load it like this
 
 # For built-in data, there's often a 'help file'
 ?iris
+iris
+data("mtcars")
 
 # "Iris" is a 'data frame.' 
 # Data frames are 2-dimensional (think Excel spreadsheet)
@@ -57,12 +69,17 @@ dim(dat)
 head(dat)
 
 # You can access specific columns of a "data frame" by name using '$'
-dat$Species
+test <- dat$Species
+test2 <- dat$Sepal.Length
 dat$Sepal.Length
+
+str(dat)
 
 # You can also use square brackets to get specific 1-D or 2-D subsets of a data frame (rows and/or columns)
 dat[1,1] # [Rows, Columns]
 dat[1:3,5]
+data.frame[rows,columns]
+
 
 # Plotting ####
 
@@ -86,7 +103,7 @@ str(dat)
 
 # Let's try
 nums <- c(1,1,2,2,2,2,3,3,3,4,4,4,4,4,4,4,5,6,7,8,9)
-class(nums) # make sure it's numeric
+class(nums) # make sure it's numeric=class
 
 # convert to a factor
 as.factor(nums) # show in console
@@ -125,7 +142,20 @@ data.frame(Clothes = col1, Numbers = col2, Factor_numbers = col3) # colname = ve
 df1 = data.frame(Clothes = col1, Numbers = col2, Factor_numbers = col3) # assign to df1
 df1 # look at it...note column names are what we gave it.
 
+max(vector1)
+min(vector1)
+median(vector1)
+mean(vector1)
+range(vector1)
+summary(vector1)
 
+# cumulative functions
+cumsum(vector1)
+cumprod(vector1)
+cummin(vector1)
+cummax(vector1)
+
+dbinom
 
 
 # Practice subsetting ####
@@ -163,21 +193,49 @@ for(i in levels(dat$Species)){
   print(mean(dat[dat$Species == i,"Sepal.Length"]))
 }
 
+mtcars
+iris
 
-
+install.packages("ggplot2")
 # YOUR REMAINING HOMEWORK ASSIGNMENT (Fill in with code) ####
 
-# 1.  Make a scatterplot of Sepal.Length vs Sepal.Width. See if you can get the points to be colored by "Species"
+# 1.  Get a subset of the "iris" data frame where it's just even-numbered rows
+
+seq(2,150,2) # here's the code to get a list of the even numbers between 2 and 150
+iris.file <- iris  # here I named the file to include the data in the file 
+iris.file[seq(2,150,2),]  # here we got a subset of data from the iris file of all even numbered rows 
+
+# 2.  Create a new object called iris_chr which is a copy of iris, except where every column is a character class
+iris$Sepal.Length <- as.character(iris$Sepal.Length)
+iris$Sepal.Width <- as.character(iris$Sepal.Width)
+iris$Petal.Length <- as.character(iris$Petal.Length)
+iris$Petal.Width <- as.character(iris$Petal.Width)
+iris$Species <- as.character(iris$Species)
+
+iris_chr <- c(iris$Sepal.Length, iris$Sepal.Width, iris$Petal.Length, iris$Petal.Width, iris$Species)
+# created a new file
+
+# 3.  Create a new numeric vector object named "Sepal.Area" which is the product of Sepal.Length and Sepal.Width
+Sepal.Area <- as.numeric(iris$Sepal.Length)*as.numeric(iris$Sepal.Width)
+
+iris <- dat
+#  I had to do this extra step because I messed up on number 4 and did  cbind with only sepal area and it  got rid of all my data, besides sepal area
+
+# 4.  Add Sepal.Area to the iris data frame as a new column
+iris.file <- cbind(Sepal.Area,iris)
 
 
-# 2.  Write the code to save it (with meaningful labels) as a jpeg file
+# 5.  Create a new dataframe that is a subset of iris using only rows where Sepal.Area is greater than 20 
+# (name it big_area_iris)
+subset(iris.file, Sepal.Area > 20) 
+# did that to create the specific subset that included that area 
+big_area_iris <- subset(iris.file, Sepal.Area > 20)
 
 
-# 3.  Subset the Iris data set to only include rows from the setosa and virginica Species
 
-
-# 4.  Write code to save this new subset as a .csv file called setosa_and_virginica.csv
-
-
-# 5.  Upload this R script (with all answers filled in and tasks completed) to canvas and GitHub
-      # I should be able to run your R script and get all the plots created and saved, etc.
+# 6.  Upload the last numbered section of this R script (with all answers filled in and tasks completed) 
+# to canvas
+# I should be able to run your R script and get all the right objects generated
+this is my assignment3
+This is Assingment #3
+This is my assingment #3
